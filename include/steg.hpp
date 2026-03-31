422792d33fd759b6785cf4457932ca75ff2cb0a3
guymo <guymolnar2000@gmail.com>
2026-03-31 18:21:22 +0300

    Fixed compilation error by moving the STB_IMAGE implementations declerations to the .cpp file

--- include/steg.hpp
+++ include/steg.hpp
@@ -1,10 +1,6 @@
 #pragma once
 #include <vector>
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include <cstdint>
 
 void embedData(const std::vector<uint8_t>& data, const char* imagePath, const char* outputPath);
 std::vector<uint8_t> extractData(const char* imagePath);
